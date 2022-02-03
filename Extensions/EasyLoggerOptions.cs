c6a60dc3ad17e3bbb16ab840b71662d0ad6c1cf6
07prajwal2000 <manuaradhya07@gmail.com>
2022-02-03 16:02:31 +0530

    Added support for Microsoft's DI framework

---
+++ Extensions/EasyLoggerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyLogger.Extensions
+{
+    public class EasyLoggerOptions
+    {
+        public string FilePath { get; set; }
+        public LogOutputType LogOutputType { get; set; } = LogOutputType.Console;
+    }
+}
