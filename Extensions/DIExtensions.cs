81da2e37aec62ebdf55eeafc1f3b727e0220a4b0
07prajwal2000 <manuaradhya07@gmail.com>
2022-02-03 16:02:26 +0530

    Added support for Microsoft's DI framework

---
+++ Extensions/DIExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyLogger.Extensions
+{
+    public static class DIExtensions
+    {
+        public static void AddEasyLogger(this IServiceCollection services, 
+            Action<EasyLoggerOptions> ConfigureOptions)
+        {
+            var options = new EasyLoggerOptions();
+            ConfigureOptions?.Invoke(options);
+
+            switch (options.LogOutputType)
+            {
+                case LogOutputType.WriteToFile:
+                    services.AddSingleton<IEasyLogger, EasyFileLogger>(e =>
+                        new EasyFileLogger(options.FilePath));
+                    break;
+                
+                case LogOutputType.Console:
+                    services.AddSingleton<IEasyLogger, EasyConsoleLogger>(e =>
+                        new EasyConsoleLogger());
+                    break;
+            }
+        }
+    }
+}
